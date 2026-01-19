c93f0a981e31a4de524896595faa5c6408ae6fb3
root <root@d3xtros.aardwolf-ghoul.ts.net>
2026-01-19 04:59:06 +0000

    Jan 18 2025

---
+++ Arrays/sort012.cpp
@@ -0,0 +1,33 @@
+// Date: Sunday, Jan 18 2025
+// LeetCode #75 - Sort Colors
+// Difficulty: Medium
+// Technique: Dutch National Flag Algorithm
+// Intuition: https://www.youtube.com/watch?v=9pdkbqGwUhs
+// Time Complexity: O(n), Space Complexity: O(1)
+
+class Solution {
+    public:
+	int sortColors(vector<int>& nums)
+	{
+	    int low = 0, mid = 0, high = nums.size() - 1;
+	    while(mid <= high)
+	    {
+ 		if (nums[mid] == 0)
+		{
+		    swap(nums[low], nums[mid]);
+		    low++;
+		    mid++;
+		}
+		else if(nums[mid] == 1)
+		{
+		    mid++;
+		}
+		else 
+		{
+		    swap(nums[mid], nums[high]);
+		    high--;
+		}
+	    }
+	}
+
+}
