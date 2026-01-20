7c990e541bc8bacc1dd634864b3a895b64598604
root <root@d3xtros.aardwolf-ghoul.ts.net>
2026-01-20 05:46:56 +0000

    Jan 19 2026

---
+++ Arrays/majorityElement.cpp
@@ -0,0 +1,35 @@
+// Date: Monday, Jan 19, 2025
+// LeetCode #169: Majority Element
+// Difficulty: Easy/Medium (Medium if using optimal solution; find below)
+// Technique: Moore's Voting Algorithm
+// Intuition: https://www.youtube.com/watch?v=nP_ns3uSh80
+// Time Complexity: O(n), Space Complexity: O(1)
+
+class Solution 
+{
+    public:
+    int majorityElement(vector<int>& nums) 
+    {
+	int el = nums[0];
+	int count = 1;
+
+	for (int i = 1; i < nums.size(); i++) 
+	{
+	    if (count == 0) 
+	    {
+		el = nums[i];
+		count = 0;
+	    }
+	    else if (nums[i] == el) 
+	    {
+		count++;
+	    }
+	    else 
+	    {
+		count--;
+	    }
+	}
+	return el;
+    }
+}
+
