cb66cd3e40007305008f1e75e3ffe1a1efd0f703
root <root@d3xtros.aardwolf-ghoul.ts.net>
2026-01-21 03:05:18 +0000

    Fix linter errors

--- Arrays/majorityElement.cpp
+++ Arrays/majorityElement.cpp
@@ -4,32 +4,23 @@
 // Technique: Moore's Voting Algorithm
 // Intuition: https://www.youtube.com/watch?v=nP_ns3uSh80
 // Time Complexity: O(n), Space Complexity: O(1)
+#include <bits/stdc++.h>
+class Solution {
+public:
+  int majorityElement(std::vector<int> &nums) {
+    int el = nums[0];
+    int count = 1;
 
-class Solution 
-{
-    public:
-    int majorityElement(vector<int>& nums) 
-    {
-	int el = nums[0];
-	int count = 1;
-
-	for (int i = 1; i < nums.size(); i++) 
-	{
-	    if (count == 0) 
-	    {
-		el = nums[i];
-		count = 0;
-	    }
-	    else if (nums[i] == el) 
-	    {
-		count++;
-	    }
-	    else 
-	    {
-		count--;
-	    }
-	}
-	return el;
+    for (int i = 1; i < nums.size(); i++) {
+      if (count == 0) {
+        el = nums[i];
+        count = 0;
+      } else if (nums[i] == el) {
+        count++;
+      } else {
+        count--;
+      }
     }
-}
-
+    return el;
+  }
+};
--- Arrays/sort012.cpp
+++ Arrays/sort012.cpp
@@ -1,33 +1,27 @@
-// Date: Sunday, Jan 18, 2026 
+// Date: Sunday, Jan 18, 2026
 // LeetCode #75 - Sort Colors
 // Difficulty: Medium
 // Technique: Dutch National Flag Algorithm
 // Intuition: https://www.youtube.com/watch?v=9pdkbqGwUhs
 // Time Complexity: O(n), Space Complexity: O(1)
+#include <bits/stdc++.h>
 
+using namespace std;
 class Solution {
-    public:
-	int sortColors(vector<int>& nums)
-	{
-	    int low = 0, mid = 0, high = nums.size() - 1;
-	    while(mid <= high)
-	    {
- 		if (nums[mid] == 0)
-		{
-		    swap(nums[low], nums[mid]);
-		    low++;
-		    mid++;
-		}
-		else if(nums[mid] == 1)
-		{
-		    mid++;
-		}
-		else 
-		{
-		    swap(nums[mid], nums[high]);
-		    high--;
-		}
-	    }
-	}
-
-}
+public:
+  void sortColors(vector<int> &nums) {
+    int low = 0, mid = 0, high = nums.size() - 1;
+    while (mid <= high) {
+      if (nums[mid] == 0) {
+        swap(nums[low], nums[mid]);
+        low++;
+        mid++;
+      } else if (nums[mid] == 1) {
+        mid++;
+      } else {
+        swap(nums[mid], nums[high]);
+        high--;
+      }
+    }
+  }
+};
